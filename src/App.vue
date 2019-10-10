2cf109ceb2e1b600c65d37cfc25d2fe33f7c369f
Maurice Pittet <maurice.pittet@lausanne.ch>
2019-10-10 18:26:38 +0200

    Test of Employe component

--- src/App.vue
+++ src/App.vue
@@ -17,17 +17,20 @@
 
     <v-content>
       <HelloWorld/>
+      <Employe></Employe>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import Employe from './components/Employe'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    Employe
   },
   data: () => ({
     //
