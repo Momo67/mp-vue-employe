58e4fc8965d1f2be098ac86e023b6ccf43422346
Maurice Pittet <maurice.pittet@lausanne.ch>
2022-07-27 16:24:10 +0200

    Bug correction when an email adress contains an hyphen

--- src/components/accents.js
+++ src/components/accents.js
@@ -12,7 +12,9 @@ export const removeAccents = (s) => {
     r = r.replace(new RegExp("œ", 'g'),"oe");
     r = r.replace(new RegExp("[ùúûü]", 'g'),"u");
     r = r.replace(new RegExp("[ýÿ]", 'g'),"y");
-    r = r.replace(new RegExp("\\W", 'g'),"");
+    r = r.replace(new RegExp("\\W^-", 'g'),"");
+    r = r.replace(new RegExp("@", 'g'),"");
+    r = r.replace(new RegExp("#", 'g'),"");
     return r;
   }
   else
